4c0a9f527a4e6e72c53dcccbb7fc9e8980665525
D3SOX <nico@d3sox.me>
2026-01-25 10:12:03 +0100

    fix: add domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://opentubex.org',
 	integrations: [
 		starlight({
 			title: 'OpenTubeX',
